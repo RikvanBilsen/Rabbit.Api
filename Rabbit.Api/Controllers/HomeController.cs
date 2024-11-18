4a8751bbd71f4d76832a450c0f8cb8fdcbd8a3e2
Rik van Bilsen <rikvanb02@gmail.com>
2024-11-18 13:56:59 +0100

    Basic controller functions added: get posts and Create post

--- Rabbit.Api/Controllers/HomeController.cs
+++ Rabbit.Api/Controllers/HomeController.cs
@@ -1,6 +1,41 @@
-﻿namespace Rabbit.Api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rabbit.Api.Data;
+using Rabbit.Api.Models;
+
+namespace Rabbit.Api.Controllers
 {
-    public class HomeController
+    [ApiController]
+    [Route("api/post")]
+    public class ArticleController : ControllerBase
     {
+        private readonly DatabaseContext _context;
+
+        public ArticleController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Post>>> GetAllPosts()
+        {
+            var posts = await _context.Posts.ToListAsync();
+
+            return Ok(posts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost([FromBody] Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAllPosts), new { id = post.PostId }, post);
+        }
     }
 }
